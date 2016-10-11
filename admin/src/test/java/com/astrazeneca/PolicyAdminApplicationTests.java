9fd44313f1ca8a33e0b562d39adb1bdb3dc37587
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-11 09:24:20 -0500

    Fixed client and admin apps for distribution

--- admin/src/test/java/com/astrazeneca/PolicyAdminApplicationTests.java
+++ admin/src/test/java/com/astrazeneca/PolicyAdminApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudoadminApplicationTests {
+public class PolicyAdminApplicationTests {
 
 	@Test
 	public void contextLoads() {
