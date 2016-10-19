e533b1ec6921e03d49dde26cabf39993a426708e
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-19 16:09:10 -0500

    Updated gitignore

--- client/src/pages/search/search.ts
+++ client/src/pages/search/search.ts
@@ -6,16 +6,6 @@ import {Policies} from '../../providers/policies/policies';
   templateUrl: 'search.html'
 })
 export class Search {
-  items: Object[];
-
   constructor(private policiesService: Policies) {
-  }
-
-  getItems(ev: any) {
-    let val = ev.target.value;
-    this.policiesService.searchItem(val).then((results: Object[]) => {
-      this.items = results;
-      console.log("results: " + results);
-    });
   }
 }
