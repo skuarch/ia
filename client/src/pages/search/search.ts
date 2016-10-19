3e055c2d93d4bcc9c91f9b93e0cc4cffcfe5af9b
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-19 16:19:59 -0500

    Removed unused code

--- client/src/pages/search/search.ts
+++ client/src/pages/search/search.ts
@@ -1,11 +1,10 @@
 import {Component} from "@angular/core";
-import {Policies} from '../../providers/policies/policies';
 
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html'
 })
 export class Search {
-  constructor(private policiesService: Policies) {
+  constructor() {
   }
 }
--- client/src/pages/summaries/summaries.ts
+++ client/src/pages/summaries/summaries.ts
@@ -1,11 +1,10 @@
 import {Component} from "@angular/core";
-import {Policies} from '../../providers/policies/policies';
 
 @Component({
   selector: 'page-summaries',
   templateUrl: 'summaries.html'
 })
 export class Summaries {
-  constructor(private policiesService: Policies) {
+  constructor() {
   }
 }
