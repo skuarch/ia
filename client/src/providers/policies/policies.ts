9cd1b8822f788ff412859debc7e6e7f4d0c29afd
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-20 16:07:47 -0500

    Removed node_modules

--- client/src/providers/policies/policies.ts
+++ client/src/providers/policies/policies.ts
@@ -1,8 +1,4 @@
 import {Injectable} from '@angular/core';
-import 'rxjs/add/operator/map';
-import PouchDB from 'pouchdb';
-import PouchDB_Plugin_QuickSearch from 'pouchdb-quick-search';
-PouchDB.plugin(PouchDB_Plugin_QuickSearch);
 
 @Injectable()
 export class Policies {
