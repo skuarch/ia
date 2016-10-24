f0bf45472235fde0ee94602fddbd12e4ae25c2d5
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-24 14:24:22 -0500

    GPOL-35 Added latest news logic

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,11 +1,28 @@
 import {Component} from "@angular/core";
-import {News} from '../../providers/news/news';
+import { Platform } from 'ionic-angular';
+import {News} from '../../model/news/news';
+import {NewsProvider} from '../../providers/news/newsProvider';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class Home {
-  constructor(public newsService : News) {
+  latestNews: News;
+
+  constructor(private newsService : NewsProvider, public platform: Platform) {
+    this.initHome();
+  }
+
+  initHome() {
+    this.platform.ready().then(() => {
+      this.initNews();
+    });
+  }
+
+  initNews() {
+    this.newsService.getLatest().then((news) => {
+      this.latestNews = news;
+    });
   }
 }
