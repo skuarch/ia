65b9c5e01b5c8b84b67a090cfdf11134c55d3f48
jose.bello@astrazeneca.com <jose.bello@astrazeneca.com>
2016-10-25 16:17:32 -0500

    GPOL-34 testing menu subsections

--- client/src/pages/summaries/summaries.ts
+++ client/src/pages/summaries/summaries.ts
@@ -4,7 +4,59 @@ import {Component} from "@angular/core";
   selector: 'page-summaries',
   templateUrl: 'summaries.html'
 })
-export class Summaries {
+export class Summaries {   
+
+    public policies: string = `
+[{
+  "group":{
+    "name": "group 1",
+     "id": 1,
+     "subGroup":[{
+       "name": "sub group name 1",
+       "id": 1,
+       "policy":
+       [
+         {
+           "title":"policy title 1",
+           "id": 1
+         },
+         {
+           "title":"policy title 2",
+           "id": 2
+         }
+       ]
+    },
+    {
+       "name": "sub group name 2",
+       "id": 1,
+       "policy":
+       [
+         {
+           "title":"policy title 5",
+           "id": 1
+         },
+         {
+           "title":"policy title 6",
+           "id": 2
+         }
+       ]
+    }],
+    "policies": [
+      {
+        "title":"policy title 3",
+        "id": 3
+      },
+      {
+        "title":"policy title 4",
+        "id": 4
+      }
+    ]
+  }          
+}]
+`;
+
   constructor() {
+      this.policies = JSON.parse(this.policies)	;
   }
+
 }
