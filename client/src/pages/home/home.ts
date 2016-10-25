bc67c066b300ec52141814cf7e9777553a567085
Juan Castrejon <juan.castrejon@astrazeneca.com>
2016-10-25 11:26:00 -0500

    GPOL-35 Show offline/online content

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+import { Component, NgZone } from "@angular/core";
 import { Platform } from 'ionic-angular';
+import { Network } from 'ionic-native';
+
 import {News} from '../../model/news/news';
 import {NewsProvider} from '../../providers/news/newsProvider';
 
@@ -9,14 +11,16 @@ import {NewsProvider} from '../../providers/news/newsProvider';
 })
 export class Home {
   latestNews: News;
+  hasConnection: boolean;
 
-  constructor(private newsService : NewsProvider, public platform: Platform) {
+  constructor(private newsService : NewsProvider, public platform: Platform, public zone: NgZone) {
     this.initHome();
   }
 
   initHome() {
     this.platform.ready().then(() => {
       this.initNews();
+      this.initConnectivity();
     });
   }
 
@@ -24,5 +28,32 @@ export class Home {
     this.newsService.getLatest().then((news) => {
       this.latestNews = news;
     });
+  }
+
+  initConnectivity() {
+    this.platform.ready().then(x => {
+      Network.onDisconnect().subscribe(x => {
+        this.zone.run(() => { this.onNetworkDisconnect(); });
+      });
+
+      Network.onConnect().subscribe(x => {
+        this.zone.run(() => { this.onNetworkConnect(); });
+      });
+
+      this.hasConnection = Network.connection != 'none';
+    });
+  }
+
+  private onNetworkDisconnect(): void {
+    this.hasConnection = false;
+    console.log('Connection lost!', this.hasConnection, Network.connection);
+  }
+
+  private onNetworkConnect(): void {
+    let self = this;
+    setTimeout(function () {
+      self.hasConnection = Network.connection != 'none';
+      console.log('Connection restored!', self.hasConnection, Network.connection);
+    }, 2000);
   }
 }
