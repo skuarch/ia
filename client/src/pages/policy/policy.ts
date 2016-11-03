f757fe2f533121bcd94f5307500c5ba436c0b5bc
jose.bello@astrazeneca.com <jose.bello@astrazeneca.com>
2016-11-03 08:15:38 -0600

    now the policies inside of a subsection are hidden

--- client/src/pages/policy/policy.ts
+++ client/src/pages/policy/policy.ts
@@ -7,8 +7,7 @@ import { PolicyDetails } from './policy-details';
 })
 export class Policy {
 
-    public array: string;
-    public isPolicyDisplayed = false;
+    public array: string;        
 
     constructor(public modalCtrl: ModalController){
        this.array =
@@ -21,23 +20,59 @@ export class Policy {
         [
             {
                 "name":"subsection name 1",
-                "id": 1,
-                "policies":
-                [
-                    {
-                        "title": "pol sub 1",
-                        "id": 10
-                    }
-                ]
-            },
-            {
-                "name":"subsection name 2",
                 "id": 2,
                 "policies":
                 [
                     {
+                        "title": "pol sub 1",
+                        "id": 3
+                    }
+                ],
+                "subsections":
+                [
+                  {
+                    "name":"sub-sub-section name 1",
+                    "id": 22,
+                    "policies":
+                     [
+                       {
+                         "title": "pol sub sub 2",
+                         "id": 23
+                       }
+                     ]
+                   },
+                   {
+                    "name":"sub-sub-section name 2",
+                    "id": 24,
+                    "policies":
+                     [
+                       {
+                         "title": "pol sub sub 4",
+                         "id": 25
+                       }
+                     ]
+                   }        
+                ]
+            },
+            {
+                "name":"subsection name 2",
+                "id": 4,
+                "policies":
+                [
+                    {
                         "title": "pol sub 2",
-                        "id": 10
+                        "id": 5
+                    }
+                ]
+            },
+            {
+                "name":"subsection name 3",
+                "id": 20,
+                "policies":
+                [
+                    {
+                        "title": "pol sub 5",
+                        "id": 21
                     }
                 ]
             }            
@@ -46,34 +81,34 @@ export class Policy {
         [
             {
                 "title": "policy sec 1",
-                "id": 1
+                "id": 6
             }
         ]        
     },
     {
         "name": "section name 2",
-        "id": 1,
+        "id": 7,
         "subsections":
         [
             {
                 "name":"subsection name 2",
-                "id": 1,
+                "id": 8,
                 "policies":
                 [
                     {
                         "title": "pol sub 2",
-                        "id": 10
+                        "id": 9
                     }
                 ]
             },
             {
                 "name":"subsection name 3",
-                "id": 2,
+                "id": 10,
                 "policies":
                 [
                     {
                         "title": "pol sub 3",
-                        "id": 10
+                        "id": 11
                     }
                 ]
             }            
@@ -82,34 +117,34 @@ export class Policy {
         [
             {
                 "title": "pol sec 2",
-                "id": 1
+                "id": 12
             }
         ]        
     },
     {
         "name": "section name 3",
-        "id": 1,
+        "id": 13,
         "subsections":
         [
             {
                 "name":"subsection name 1",
-                "id": 1,
+                "id": 14,
                 "policies":
                 [
                     {
                         "title": "pol sub 1",
-                        "id": 10
+                        "id": 15
                     }
                 ]
             },
             {
                 "name":"subsection name 2",
-                "id": 2,
+                "id": 16,
                 "policies":
                 [
                     {
                         "title": "pol sub 2",
-                        "id": 10
+                        "id": 17
                     }
                 ]
             }            
@@ -118,8 +153,13 @@ export class Policy {
         [
             {
                 "title": "pol sec 3",
-                "id": 1
+                "id": 18
+            },
+            {
+                "title": "pol sec 4",
+                "id": 19
             }
+
         ]        
     }
     
@@ -134,21 +174,45 @@ this.array = JSON.parse(this.array);
     modal.present();
   }
 
-  showPolicies(subsection){
+  showPolicies(subsection){ 
 
-      if(this.isPolicyDisplayed){
-          this.isPolicyDisplayed = false;
-      }else{
-          this.isPolicyDisplayed = true;
-      }
-
-      console.log("se deben de mostrar las politicas", subsection);
+      console.log('showPolicies subsection ', subsection);     
       var policies: any = subsection.policies;
-      for(let policy of policies){   
-          let element = 'policy' + policy.id;
-          let button = document.getElementById('' + element + '');                    
-          console.log("button ",button);
+      for(let policy of policies){
+
+          let element = 'policy' + policy.id;console.log('elemetn',element);
+          let button: HTMLElement = document.getElementById('' + element + '');
+          let buttonStyle: any = button.style;
+
+          if(buttonStyle.display == 'inline'){
+            button.style.display = "none";            
+          }else{
+            button.style.display = "inline";              
+          }
+          
       }
+  }
+
+
+  showSubSections(subsection){
+
+    console.log('showSubSections subsection ', subsection);
+
+    var subsections: any = subsection.subsections;
+      for(let subsection of subsections){
+
+          let element = 'subsub' + subsection.id;
+          let list: HTMLElement = document.getElementById('' + element + '');
+          let listStyle: any = list.style;
+
+          if(listStyle.display == 'inline'){
+            list.style.display = "none";            
+          }else{
+            list.style.display = "inline";              
+          }
+          
+      }
+
   }
   
 }
